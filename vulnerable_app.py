7217c41f920459f2b7dc47e0fe0a74677bb08b32
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2023-11-15 11:30:11 -0500

    added /app_fixed

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -41,6 +41,32 @@ def index():
                            currency=user.Currency,
                            message=withdraw_msg)
 
+@app.route('/app_fixed', methods=['GET', 'POST'])
+def app_fixed():
+    if request.method == 'GET':
+        return render_template('app_fixed.html',
+                               username=user.User,
+                               balance=str(user.Balance),
+                               currency=user.Currency,
+                               message='')
+
+    try:
+        withdrawAmount = float(request.form.get('amount'))
+        if withdrawAmount > 0 and withdrawAmount <= user.Balance:
+            user.Withdraw(withdrawAmount)
+            withdraw_msg = f'You withdrew {user.Currency} {str(withdrawAmount)}!'
+        else:
+            withdraw_msg = 'Withdrawal failed: Invalid amount or insufficient balance.'
+    except ValueError:
+        withdraw_msg = 'Invalid input: Please enter a valid number.'
+    except:
+        withdraw_msg = 'Withdrawal failed: Unknown error occurred.'
+
+    return render_template('app_fixed.html',
+                           username=user.User,
+                           balance=str(user.Balance),
+                           currency=user.Currency,
+                           message=withdraw_msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
